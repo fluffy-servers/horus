d2608d07a50553672091cbecf29396fa6a1d04c2
Robert Fraser <robert.fraser@uqconnect.edu.au>
2020-10-15 23:06:19 +1000

    Fix errors with missing powertool args

--- lua/horus/commands/powertools.lua
+++ lua/horus/commands/powertools.lua
@@ -1,9 +1,11 @@
 horus.command("rcon", "Run a given command in server console", {"string:command"}, function(caller, cmd)
+	if not cmd then return false, "No command specified" end
 	game.ConsoleCommand(cmd .. "\n")
 	return true, "%c ran command %1 on the server"
 end)
 
 horus.command("lua", "Run a given Lua script on the server", {"string:command"}, function(caller, cmd)
+	if not cmd then return false, "No command specified" end
 	RunString(cmd)
 	return true, "%c ran Lua code"
 end)
